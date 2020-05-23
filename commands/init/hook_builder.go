e642fe6989398da983dc69877b809b1a264056b7
Evgeny Abramovich <evg.abramovitch@gmail.com>
2020-05-23 19:39:13 +0300

    Added tests for build hook function

--- commands/init/hook_builder.go
+++ commands/init/hook_builder.go
@@ -3,5 +3,16 @@ package init
 import "strings"
 
 func buildHook(fishermanCommand, hookName string) string {
-	return strings.Join([]string{fishermanCommand, "handle", "--hook", hookName}, " ")
+	return rows([]string{
+		"#!/bin/sh",
+		command([]string{fishermanCommand, "handle", "--hook", hookName, "$@"}),
+	})
+}
+
+func command(params []string) string {
+	return strings.Join(params, " ")
+}
+
+func rows(rows []string) string {
+	return strings.Join(rows, "\n")
 }
---
+++ commands/init/hook_builder_test.go
@@ -0,0 +1,33 @@
+package init
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestBuildHook(t *testing.T) {
+	testData := []struct{
+		hookName string
+		expectedHook string
+	}{
+		{"applypatch-msg", "#!/bin/sh\nfisherman handle --hook applypatch-msg $@"},
+		{"commit-msg", "#!/bin/sh\nfisherman handle --hook commit-msg $@"},
+		{"fsmonitor-watchman", "#!/bin/sh\nfisherman handle --hook fsmonitor-watchman $@"},
+		{"post-update", "#!/bin/sh\nfisherman handle --hook post-update $@"},
+		{"pre-applypatch", "#!/bin/sh\nfisherman handle --hook pre-applypatch $@"},
+		{"pre-commit", "#!/bin/sh\nfisherman handle --hook pre-commit $@"},
+		{"pre-push", "#!/bin/sh\nfisherman handle --hook pre-push $@"},
+		{"pre-rebase", "#!/bin/sh\nfisherman handle --hook pre-rebase $@"},
+		{"pre-receive", "#!/bin/sh\nfisherman handle --hook pre-receive $@"},
+		{"prepare-commit-msg", "#!/bin/sh\nfisherman handle --hook prepare-commit-msg $@"},
+		{"update", "#!/bin/sh\nfisherman handle --hook update $@"},
+	}
+
+	for _, tt := range testData {
+		t.Run(fmt.Sprintf("Build %s hook", tt.hookName), func(t *testing.T) {
+			result := buildHook("fisherman", tt.hookName)
+			assert.Equal(t, result, tt.expectedHook)
+		})
+	}
+}
