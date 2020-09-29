43b33667d8d82d86e80759ea609ea0a527b831e2
546546456 <evg.abramovitch@gmail.com>
2020-09-29 19:29:34 +0300

    Fixed HooksConfig structure size

--- config/hooks.go
+++ config/hooks.go
@@ -7,7 +7,6 @@ import (
 // HooksConfig is structure for storage information for hook validation
 type HooksConfig struct {
 	ApplyPatchMsgHook     hooks.ApplyPatchMsgHookConfig     `yaml:"applypatch-msg,omitempty"`
-	CommitMsgHook         hooks.CommitMsgHookConfig         `yaml:"commit-msg,omitempty"`
 	FsMonitorWatchmanHook hooks.FsMonitorWatchmanHookConfig `yaml:"fsmonitor-watchman,omitempty"`
 	PostUpdateHook        hooks.PostUpdateHookConfig        `yaml:"post-update,omitempty"`
 	PreApplyPatchHook     hooks.PreApplyPatchHookConfig     `yaml:"pre-applypatch,omitempty"`
@@ -15,6 +14,7 @@ type HooksConfig struct {
 	PrePushHook           hooks.PrePushHookConfig           `yaml:"pre-push,omitempty"`
 	PreRebaseHook         hooks.PreRebaseHookConfig         `yaml:"pre-rebase,omitempty"`
 	PreReceiveHook        hooks.PreReceiveHookConfig        `yaml:"pre-receive,omitempty"`
-	PrepareCommitMsgHook  hooks.PrepareCommitMsgHookConfig  `yaml:"prepare-commit-msg,omitempty"`
 	UpdateHook            hooks.UpdateHookConfig            `yaml:"update,omitempty"`
+	CommitMsgHook         hooks.CommitMsgHookConfig         `yaml:"commit-msg,omitempty"`
+	PrepareCommitMsgHook  hooks.PrepareCommitMsgHookConfig  `yaml:"prepare-commit-msg,omitempty"`
 }
