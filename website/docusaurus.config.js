18ff486f22f40d14e04bca4418166e6c6e390fcd
Evgeny Abramovich <evg.abramovitch@gmail.com>
2020-10-03 02:41:44 +0300

    updated styles

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,35 +1,20 @@
 module.exports = {
   title: 'fisherman',
   tagline: 'Small git hooks tool for developer.',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://fisherman.netlify.app',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   favicon: 'img/favicon.ico',
-  organizationName: 'evg4b', // Usually your GitHub org/user name.
-  projectName: 'fisherman', // Usually your repo name.
+  organizationName: 'evg4b',
+  projectName: 'fisherman',
   themeConfig: {
     navbar: {
       title: 'fisherman',
-      logo: {
-        alt: 'fisherman logeo',
-        src: 'img/logo.svg',
-      },
+      logo: { alt: 'fisherman logo', src: 'img/logo.png' },
       items: [
-        {
-          type: 'docsVersionDropdown',
-          position: 'left',
-        },
-        {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'left',
-        },
-        {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
-        },
+        { type: 'docsVersionDropdown', position: 'left' },
+        { to: 'docs/', activeBasePath: 'docs', label: 'Docs', position: 'right' },
+        { href: 'https://github.com/evg4b/fisherman', label: 'GitHub', position: 'right' },
       ],
     },
     footer: {
@@ -38,48 +23,26 @@ module.exports = {
         {
           title: 'Docs',
           items: [
-            {
-              label: 'Style Guide',
-              to: 'docs/',
-            },
-            {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
-            },
+            { label: 'Style Guide', to: 'docs/' },
+            { label: 'Second Doc', to: 'docs/doc2/' },
           ],
         },
         {
           title: 'Community',
           items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
+            { label: 'Stack Overflow', href: 'https://stackoverflow.com/questions/tagged/docusaurus' },
+            { label: 'Discord', href: 'https://discordapp.com/invite/docusaurus' },
+            { label: 'Twitter', href: 'https://twitter.com/docusaurus' },
           ],
         },
         {
           title: 'More',
           items: [
-            {
-              label: 'Blog',
-              to: 'blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
+            { label: 'Blog', to: 'blog' },
+            { label: 'GitHub', href: 'https://github.com/facebook/docusaurus' },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
   },
   presets: [
@@ -89,8 +52,7 @@ module.exports = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+          editUrl: 'https://github.com/evg4b/fisherman/edit/master/website/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
@@ -98,4 +60,15 @@ module.exports = {
       },
     ],
   ],
+  plugins: [
+    [
+      '@docusaurus/plugin-sitemap',
+      {
+        cacheTime: 600 * 1000,
+        changefreq: 'weekly',
+        priority: 0.5,
+        trailingSlash: false,
+      },
+    ],
+  ]
 };
