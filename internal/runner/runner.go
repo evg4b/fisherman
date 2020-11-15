0aa846d0102468496d56c89143f98a179dfbfb4a
Evgeny Abramovich <evg.abramovitch@gmail.com>
2020-11-15 16:25:22 +0300

    Fixed hooks registration

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -2,16 +2,14 @@ package runner
 
 import (
 	"fisherman/commands"
-	"fisherman/config"
 	"fisherman/internal"
 )
 
 type Runner struct {
 	commands []commands.CliCommand
-	config   *config.FishermanConfig
 	app      *internal.AppInfo
 }
 
-func NewRunner(commands []commands.CliCommand, config *config.FishermanConfig, app *internal.AppInfo) *Runner {
-	return &Runner{commands, config, app}
+func NewRunner(commands []commands.CliCommand, app *internal.AppInfo) *Runner {
+	return &Runner{commands, app}
 }
--- main.go
+++ main.go
@@ -34,58 +34,72 @@ func main() {
 	cwd, err := os.Getwd()
 	utils.HandleCriticalError(err)
 
-	appPath, err := os.Executable()
+	executable, err := os.Executable()
 	utils.HandleCriticalError(err)
 
-	conf, configInfo, err := config.Load(cwd, usr, filesystem.NewLocalFileSystem())
+	configuration, configInfo, err := config.Load(cwd, usr, filesystem.NewLocalFileSystem())
 	utils.HandleCriticalError(err)
 
-	log.Configure(conf.Output)
+	log.Configure(configuration.Output)
 
 	ctx := context.Background()
-
-	files := filesystem.NewLocalFileSystem()
-	sh := shell.NewShell(os.Stdout)
+	fileSystem := filesystem.NewLocalFileSystem()
+	sysShell := shell.NewShell(os.Stdout)
 	repository := vcs.NewGitRepository(cwd)
+	compiler := configcompiler.NewCompiler(repository, configuration.GlobalVariables, cwd)
 
 	factory := func(args []string, output io.Writer) *internal.Context {
-		return internal.NewInternalContext(ctx, files, sh, repository, args, output)
+		return internal.NewInternalContext(ctx, fileSystem, sysShell, repository, args, output)
 	}
 
-	compiler := configcompiler.NewCompiler(repository, conf.GlobalVariables, cwd)
-
 	hooksHandles := hooks.HandlerList{
-		constants.CommitMsgHook:         hooks.CommitMsg(factory, conf.Hooks.CommitMsgHook, compiler),
-		constants.PreCommitHook:         hooks.PreCommit(factory, conf.Hooks.PreCommitHook, sh, compiler),
+		constants.CommitMsgHook: hooks.CommitMsg(
+			factory,
+			configuration.Hooks.CommitMsgHook,
+			compiler,
+		),
+		constants.PreCommitHook: hooks.PreCommit(
+			factory,
+			configuration.Hooks.PreCommitHook,
+			sysShell,
+			compiler,
+		),
+		constants.PrePushHook: hooks.PrePush(
+			factory,
+			configuration.Hooks.PrePushHook,
+			sysShell,
+			compiler,
+		),
+		constants.PrepareCommitMsgHook: hooks.PrepareCommitMsg(
+			factory,
+			configuration.Hooks.PrepareCommitMsgHook,
+			compiler,
+		),
 		constants.ApplyPatchMsgHook:     hooks.NotSupported,
 		constants.FsMonitorWatchmanHook: hooks.NotSupported,
 		constants.PostUpdateHook:        hooks.NotSupported,
 		constants.PreApplyPatchHook:     hooks.NotSupported,
-		constants.PrePushHook:           hooks.PrePush(factory, conf.Hooks.PrePushHook, sh, compiler),
 		constants.PreRebaseHook:         hooks.NotSupported,
 		constants.PreReceiveHook:        hooks.NotSupported,
-		constants.PrepareCommitMsgHook:  hooks.PrepareCommitMsg(factory, conf.Hooks.PrepareCommitMsgHook, compiler),
 		constants.UpdateHook:            hooks.NotSupported,
 	}
 
 	appInfo := internal.AppInfo{
-		Executable:       appPath,
+		Executable:       executable,
 		Cwd:              cwd,
 		GlobalConfigPath: configInfo.GlobalConfigPath,
 		LocalConfigPath:  configInfo.LocalConfigPath,
 		RepoConfigPath:   configInfo.RepoConfigPath,
 	}
 
-	instance := runner.NewRunner(
-		[]commands.CliCommand{
-			initialize.NewCommand(files, &appInfo, usr),
-			handle.NewCommand(hooksHandles, &conf.Hooks, &appInfo),
-			remove.NewCommand(files, &appInfo, usr),
-			version.NewCommand(),
-		},
-		conf,
-		&appInfo)
+	commands := []commands.CliCommand{
+		initialize.NewCommand(fileSystem, &appInfo, usr),
+		handle.NewCommand(hooksHandles, &configuration.Hooks, &appInfo),
+		remove.NewCommand(fileSystem, &appInfo, usr),
+		version.NewCommand(),
+	}
 
+	instance := runner.NewRunner(commands, &appInfo)
 	if err = instance.Run(os.Args[1:]); err != nil {
 		panic(err)
 	}
