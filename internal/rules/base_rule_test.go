72297ee0ff620ceeed1a678dbeeffd1c33f9a9eb
Evgeny Abramovich <evg.abramovitch@gmail.com>
2021-01-12 19:18:43 +0300

    Added tests for base rule

---
+++ internal/rules/base_rule_test.go
@@ -0,0 +1,19 @@
+package rules
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBaseRule_GetType(t *testing.T) {
+	rule := BaseRule{Type: "demo-rule", Condition: "rule-condition"}
+
+	assert.Equal(t, "demo-rule", rule.GetType())
+}
+
+func TestBaseRule_GetContition(t *testing.T) {
+	rule := BaseRule{Type: "demo-rule", Condition: "rule-condition"}
+
+	assert.Equal(t, "rule-condition", rule.GetContition())
+}
