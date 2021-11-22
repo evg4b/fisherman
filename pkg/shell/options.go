669396b68e498ee336d6b5ba42bcf7ffe261c37d
Evgeny Abramovich <evg.abramovitch@gmail.com>
2021-11-22 19:50:23 +0300

    Fix documentation

--- pkg/shell/options.go
+++ pkg/shell/options.go
@@ -2,14 +2,14 @@ package shell
 
 type shellOption = func(sh *SystemShell)
 
-// WithWorkingDirectory setups working directory for shell.
+// WithWorkingDirectory setups default working directory for shell.
 func WithWorkingDirectory(cwd string) shellOption {
 	return func(sh *SystemShell) {
 		sh.cwd = cwd
 	}
 }
 
-// WithWorkingDirectory setups default shell.
+// WithDefaultShell setups default shell.
 func WithDefaultShell(defaultShell string) shellOption {
 	return func(sh *SystemShell) {
 		sh.defaultShell = defaultShell
