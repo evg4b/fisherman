59957579eda3af33e0c91332f49d6b55a5892b96
Evgeny Abramovich <evg.abramovitch@gmail.com>
2021-12-04 23:57:13 +0300

    Added base host test and docs

--- pkg/shell/README.md
+++ pkg/shell/README.md
@@ -1,9 +1,9 @@
 # fisherman/pkg/shell
 
-A package providing a simple cross-platform wrapper over the system shell.
+A package providing a lightweight cross-platform wrapper over the system shell.
 
-Main features:
-- Expandable (implements `shell.ShellStrategy` interface to integrate )
+Features:
+- Expandable (implements `shell.ShellStrategy` interface to integrate)
 - Shell host implements `io.Writer` interface to simple put commands into `stdin`.
 - The commands are executed after entering a new line. This can be used to execute
   commands over time using a single terminal process.
@@ -13,6 +13,40 @@ Main features:
 - **shell.Cmd()** - Strategy to use [cmd.exe](https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/cmd?ranMID=46131&ranEAID=a1LgFw09t88&ranSiteID=a1LgFw09t88-GnIhyB.n9js83pQZuKa7oQ&epi=a1LgFw09t88-GnIhyB.n9js83pQZuKa7oQ&irgwc=1&OCID=AID2200057_aff_7806_1243925&tduid=%28ir__tx9o062soskf6jneo33yzzo9bn2xoglw11jzryfe00%29%287806%29%281243925%29%28a1LgFw09t88-GnIhyB.n9js83pQZuKa7oQ%29%28%29&irclickid=_tx9o062soskf6jneo33yzzo9bn2xoglw11jzryfe00) as shell host. Can be used only on `windows`. It is also the default shell for windows.
 - **shell.Bash()** - Strategy to use [bash](https://www.gnu.org/software/bash/) as shell host. Available on  `windows`, `linux` and `darwin`. It is the default shell for `linux` and `darwin`.   **Note**: for windows `bash` available only in package [MSYS2](https://www.msys2.org/).
 - **shell.PowerShell()** - Strategy to use [powershell](https://docs.microsoft.com/en-us/powershell/scripting/overview?view=powershell-7.2) as shell host. Can be used on `windows`, `linux` and `darwin`.
+
+# Options:
+
+- **shell.WithCwd(dir)** - specifies the working directory of the shell. If it is the empty ot not set, host runs the shell in the calling process's current directory.
+- **shell.WithStdout(output)** - specify the process's standard output.
+- **shell.WithStderr(output)** - specify the process's standard error.
+- **shell.WithEnv(env)** - specifies the environment of the shell process (including the shell strategy provided variables).
+- **shell.WithRawEnv(env)** - specifies the environment of the shell process (without the shell strategy provided variables).
+- **shell.WithArgs(args)** - holds command line arguments (including the shell strategy provided args)
+- **shell.WithRawArgs(args)** - holds command line arguments (without the shell strategy provided args)
+
+# Encoding:
+This wrapper does not work with the shell encodings. To execute commands in a different encoding, you can wrap the host in Encoder and Stdout in Decoder from packages `golang.org/x/text/encoding/charmap` and `golang.org/x/text/transform` like this:
+
+``` GO
+cp := charmap.CodePage866 // Your encoding
+buffer := bytes.NewBufferString("")
+
+host := shell.NewHost(ctx, shell.Cmd(), shell.WithStdout(buffer))
+convertedHost := transform.NewWriter(host, cp.NewEncoder())
+
+fmt.Fprintln(convertedHost, "chcp")
+
+if err := host.Wait(); err != nil {
+  panic(err)
+}
+
+output, _, err := transform.String(cp.NewDecoder(), buff.String())
+if err != nil {
+  panic(err)
+}
+
+fmt.Print(output)
+```
 
 # Examples:
 
---
+++ pkg/shell/host_internal_test.go
@@ -0,0 +1,185 @@
+package shell
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"os/exec"
+
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewHost(t *testing.T) {
+	var buff bytes.Buffer
+
+	tests := []struct {
+		name           string
+		shellStr       ShellStrategy
+		options        []hostOption
+		expectedStdout io.Writer
+		expectedStderr io.Writer
+		expectedDir    string
+		expectedPath   string
+		expectedArgs   []string
+		expectedEnv    []string
+	}{
+		{
+			name:         "bash withut options",
+			shellStr:     Bash(),
+			options:      []func(str ShellStrategy, host *Host){},
+			expectedPath: lookupPath("bash"),
+			expectedArgs: []string{"bash", "-i"},
+		},
+		{
+			name:     "bash with raw options",
+			shellStr: Bash(),
+			options: []func(str ShellStrategy, host *Host){
+				WithCwd("/demo/test"),
+				WithRawArgs([]string{"arg1", "arg2"}),
+				WithRawEnv([]string{"VAR1=1", "VAR2=2"}),
+			},
+			expectedPath: lookupPath("bash"),
+			expectedArgs: []string{"arg1", "arg2"},
+			expectedEnv:  []string{"VAR1=1", "VAR2=2"},
+			expectedDir:  "/demo/test",
+		},
+		{
+			name:     "bash with options",
+			shellStr: Bash(),
+			options: []func(str ShellStrategy, host *Host){
+				WithCwd("/demo/test"),
+				WithArgs([]string{"arg1", "arg2"}),
+				WithEnv([]string{"VAR1=1", "VAR2=2"}),
+			},
+			expectedPath: lookupPath("bash"),
+			expectedArgs: []string{"-i", "arg1", "arg2"},
+			expectedEnv:  []string{"VAR1=1", "VAR2=2"},
+			expectedDir:  "/demo/test",
+		},
+		{
+			name:     "bash with Stdout and Stderr options",
+			shellStr: Bash(),
+			options: []func(str ShellStrategy, host *Host){
+				WithStdout(&buff),
+				WithStderr(&buff),
+			},
+			expectedPath:   lookupPath("bash"),
+			expectedArgs:   []string{"bash", "-i"},
+			expectedStdout: &buff,
+			expectedStderr: &buff,
+		},
+		{
+			name:         "cmd withut options",
+			shellStr:     Cmd(),
+			options:      []func(str ShellStrategy, host *Host){},
+			expectedPath: lookupPath("cmd"),
+			expectedArgs: []string{"cmd", "/Q", "/D", "/K"},
+		},
+		{
+			name:     "cmd with raw options",
+			shellStr: Cmd(),
+			options: []func(str ShellStrategy, host *Host){
+				WithCwd("/demo/test"),
+				WithRawArgs([]string{"arg1", "arg2"}),
+				WithRawEnv([]string{"VAR1=1", "VAR2=2"}),
+			},
+			expectedPath: lookupPath("cmd"),
+			expectedArgs: []string{"arg1", "arg2"},
+			expectedEnv:  []string{"VAR1=1", "VAR2=2"},
+			expectedDir:  "/demo/test",
+		},
+		{
+			name:     "cmd with options",
+			shellStr: Cmd(),
+			options: []func(str ShellStrategy, host *Host){
+				WithCwd("/demo/test"),
+				WithArgs([]string{"arg1", "arg2"}),
+				WithEnv([]string{"VAR1=1", "VAR2=2"}),
+			},
+			expectedPath: lookupPath("cmd"),
+			expectedArgs: []string{"/Q", "/D", "/K", "arg1", "arg2"},
+			expectedEnv:  []string{"VAR1=1", "VAR2=2"},
+			expectedDir:  "/demo/test",
+		},
+		{
+			name:     "cmd with Stdout and Stderr options",
+			shellStr: Cmd(),
+			options: []func(str ShellStrategy, host *Host){
+				WithStdout(&buff),
+				WithStderr(&buff),
+			},
+			expectedPath:   lookupPath("cmd"),
+			expectedArgs:   []string{"cmd", "/Q", "/D", "/K"},
+			expectedStdout: &buff,
+			expectedStderr: &buff,
+		},
+		{
+			name:         "powershell withut options",
+			shellStr:     PowerShell(),
+			options:      []func(str ShellStrategy, host *Host){},
+			expectedPath: lookupPath("powershell"),
+			expectedArgs: []string{"powershell", "-NoProfile", "-NonInteractive", "-NoLogo"},
+		},
+		{
+			name:     "powershell with raw options",
+			shellStr: PowerShell(),
+			options: []func(str ShellStrategy, host *Host){
+				WithCwd("/demo/test"),
+				WithRawArgs([]string{"arg1", "arg2"}),
+				WithRawEnv([]string{"VAR1=1", "VAR2=2"}),
+			},
+			expectedPath: lookupPath("powershell"),
+			expectedArgs: []string{"arg1", "arg2"},
+			expectedEnv:  []string{"VAR1=1", "VAR2=2"},
+			expectedDir:  "/demo/test",
+		},
+		{
+			name:     "powershell with options",
+			shellStr: PowerShell(),
+			options: []func(str ShellStrategy, host *Host){
+				WithCwd("/demo/test"),
+				WithArgs([]string{"arg1", "arg2"}),
+				WithEnv([]string{"VAR1=1", "VAR2=2"}),
+			},
+			expectedPath: lookupPath("powershell"),
+			expectedArgs: []string{"-NoProfile", "-NonInteractive", "-NoLogo", "arg1", "arg2"},
+			expectedEnv:  []string{"VAR1=1", "VAR2=2"},
+			expectedDir:  "/demo/test",
+		},
+		{
+			name:     "powershell with Stdout and Stderr options",
+			shellStr: PowerShell(),
+			options: []func(str ShellStrategy, host *Host){
+				WithStdout(&buff),
+				WithStderr(&buff),
+			},
+			expectedPath:   lookupPath("powershell"),
+			expectedArgs:   []string{"powershell", "-NoProfile", "-NonInteractive", "-NoLogo"},
+			expectedStdout: &buff,
+			expectedStderr: &buff,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := NewHost(context.Background(), tt.shellStr, tt.options...)
+
+			assert.Equal(t, tt.expectedPath, actual.command.Path)
+			assert.Equal(t, tt.expectedArgs, actual.command.Args)
+			assert.Equal(t, tt.expectedEnv, actual.command.Env)
+			assert.Equal(t, tt.expectedDir, actual.command.Dir)
+			assert.Equal(t, tt.expectedStderr, actual.command.Stderr)
+			assert.Equal(t, tt.expectedStdout, actual.command.Stdout)
+		})
+	}
+}
+
+func lookupPath(name string) string {
+	lp, err := exec.LookPath(name)
+	if err != nil {
+		return ""
+	}
+
+	return lp
+}
