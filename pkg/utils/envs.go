ea01e590ee0a97ca16e5fc2f199eaa39e5fa820e
Evgeny Abramovich <evg.abramovitch@gmail.com>
2021-12-06 02:47:17 +0300

    Added tests for fisherman/pkg/utils

--- pkg/utils/envs.go
+++ pkg/utils/envs.go
@@ -1,15 +1,32 @@
 package utils
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+const keyValuePartsCount = 2
 
 // MergeEnv concatenates environment variables list with passed map[string]string.
 func MergeEnv(env []string, vars map[string]string) []string {
-	newEnv := []string{}
-	newEnv = append(env, newEnv...)
-
-	for key, value := range vars {
-		newEnv = append(newEnv, fmt.Sprintf("%s=%s", key, value))
+	if len(vars) == 0 {
+		return env
 	}
 
-	return newEnv
+	parsedEnv := map[string]string{}
+	for _, variable := range env {
+		parts := strings.SplitN(variable, "=", keyValuePartsCount)
+		parsedEnv[parts[0]] = parts[1]
+	}
+
+	for key, value := range vars {
+		parsedEnv[key] = value
+	}
+
+	resultEnv := []string{}
+	for key, value := range parsedEnv {
+		resultEnv = append(resultEnv, fmt.Sprintf("%s=%s", key, value))
+	}
+
+	return resultEnv
 }
--- pkg/utils/envs_test.go
+++ pkg/utils/envs_test.go
@@ -1,1 +1,98 @@
 package utils_test
+
+import (
+	. "fisherman/pkg/utils"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMergeEnv(t *testing.T) {
+	tests := []struct {
+		name     string
+		env      []string
+		vars     map[string]string
+		expected []string
+	}{
+		{
+			name:     "empty slice and map",
+			env:      []string{},
+			vars:     map[string]string{},
+			expected: []string{},
+		},
+		{
+			name: "empty source env slice",
+			env:  []string{},
+			vars: map[string]string{
+				"VAR1": "value1",
+				"VAR2": "value2",
+			},
+			expected: []string{
+				"VAR1=value1",
+				"VAR2=value2",
+			},
+		},
+		{
+			name: "empty additional vars map",
+			env: []string{
+				"VAR1=value1",
+				"VAR2=value2",
+			},
+			vars: map[string]string{},
+			expected: []string{
+				"VAR1=value1",
+				"VAR2=value2",
+			},
+		},
+		{
+			name: "empty additional vars map",
+			env: []string{
+				"VAR1=value1",
+			},
+			vars: map[string]string{
+				"VAR2": "value2",
+			},
+			expected: []string{
+				"VAR1=value1",
+				"VAR2=value2",
+			},
+		},
+		{
+			name: "overwrite source from passed vars",
+			env: []string{
+				"VAR1=value1",
+				"VAR2=value2",
+			},
+			vars: map[string]string{
+				"VAR2": "Custom value",
+			},
+			expected: []string{
+				"VAR1=value1",
+				"VAR2=Custom value",
+			},
+		},
+		{
+			name: "correctly parse values with =",
+			env: []string{
+				"VAR1=conation2=test",
+				"VAR2=conation2=test2",
+				"VAR3=",
+			},
+			vars: map[string]string{
+				"VAR2": "Custom value",
+			},
+			expected: []string{
+				"VAR1=conation2=test",
+				"VAR2=Custom value",
+				"VAR3=",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actual := MergeEnv(tt.env, tt.vars)
+
+			assert.Equal(t, tt.expected, actual)
+		})
+	}
+}
