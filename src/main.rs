890c600db52aeca50cabdde30b9803b2a6b0a4d4
Evgeny Abramovich <evg.abramovitch@gmail.com>
2024-12-21 23:54:35 -0400

    Added base cli handlers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Initialize hooks for the repository
+    Init,
+    /// Handle a hook task
+    Handle,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Init => {
+            println!("Initializing project");
+        }
+        Commands::Handle => {
+            println!("Handling task");
+        }
+    }
 }
