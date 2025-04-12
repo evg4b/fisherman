1df9a9c1bfa86e6b5f262d531e36e3f5be4f0686
Evgeny Abramovich <evg.abramovitch@gmail.com>
2025-04-12 03:04:18 -0300

    Remove unused code

--- src/templates/mod.rs
+++ src/templates/mod.rs
@@ -1,4 +1,3 @@
-mod placeholders;
 mod errors;
 mod hashmap;
 mod vec;
@@ -7,6 +6,5 @@ mod tmpl_macro;
 
 pub use crate::templates::errors::TemplateError;
 pub use crate::templates::hashmap::replace_in_hashmap;
-pub use crate::templates::placeholders::replace_in_string;
 pub use crate::templates::template::TemplateString;
 pub use crate::templates::vec::replace_in_vac;
