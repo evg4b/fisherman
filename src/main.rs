7db493aac0b8a4507eae3343c1c25593ab62620c
Evgeny Abramovich <evg.abramovitch@gmail.com>
2026-03-22 14:53:17 -0400

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use clap::Parser;
+use anyhow::Result;
 use fisherman_core::FishermanCli;
 use fisherman_core::GitRepoContext;
 use std::env;
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<()> {
     let mut context = GitRepoContext::new(env::current_dir()?)?;
     match FishermanCli::exec(&mut context) {
         Ok(()) => Ok(()),
